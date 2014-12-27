062c45cd98946c212d2ab613a04c05a8296602d7
hrl <cxghrl@gmail.com>
2014-12-28 01:54:08 +0800

    prepare to deploy

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ application = tornado.web.Application([
 ], **site_settings)
 
 if __name__ == "__main__":
-    sockets = tornado.netutil.bind_sockets(9000)
-#    tornado.process.fork_processes(0)
+    sockets = tornado.netutil.bind_sockets(site_settings["port"])
+    tornado.process.fork_processes(0)
     server = tornado.httpserver.HTTPServer(application, xheaders=True)
     server.add_sockets(sockets)
     tornado.ioloop.IOLoop.instance().start()
